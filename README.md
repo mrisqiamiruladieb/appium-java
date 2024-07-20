b67a4301f741a7ab9f215a2e9d5c3e12f0c0520f
adieb <mrizqiamiruladieb@gmail.com>
2024-07-20 23:10:31 +0700

    feat: test automation example

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Mobile Automation (Android)
 - Go to **pom.xml** *add dependencies tag* and *add the following dependencies* to it:
     - Go to [Appium Java](https://mvnrepository.com/artifact/io.appium/java-client) and [JUnit Vintage Engine](https://mvnrepository.com/artifact/org.junit.vintage/junit-vintage-engine), *copy* the content on **the Maven navbar** both, and *paste* it inside the dependencies tag
     - Click *Load Maven Changes (logo)* to install or sync the dependency
+- **Configure** appium (_server/inspector_) & device (_emulator/real_) ([reference](https://github.com/mrisqiamiruladieb/MobileAutomation-RobotFramework/blob/main/README.md#persiapan-awal))
+  - _Tips:_ use the **record feature** in _the appium inspector_ to make it easier to get the element address
+
+## Steps
+- **Create** a script file. _Example:_ src/test/java/**mobile.java**
+- Import required _libraries_
+- _Code_ and click **run** (_the play logo_)
+
+## Issues and Solutions
+- **Error** when **Running Test**
+  - _Error Messages:_ org.openqa.selenium.SessionNotCreatedException:could not start a new session.Possible causes are invalid address of the remote server or browser start-up failure
+  - _Solution:_ **Update** _appium java_ library with **compatible** selenium ([reference](https://discuss.appium.io/t/org-openqa-selenium-sessionnotcreatedexception-could-not-start-a-new-session-possible-causes-are-invalid-address-of-the-remote-server-or-browser-start-upfailure/42105))
---
+++ src/test/java/mobile.java
@@ -0,0 +1,55 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class mobile {
+    public static AndroidDriver driver; // initialize android driver for appium
+    public static DesiredCapabilities capabilities; // initialize desired capabilities for device setup
+    public static String baseUrl = "http://127.0.0.1:4723/"; // appium server url
+
+    @Test
+    public void main() throws MalformedURLException {
+        // setup device to interact with appium server
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Rizqi's S");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "14.0");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        // set apk file directory, install, and run the app
+        capabilities.setCapability("app", "D:\\belajar\\appium-java\\src\\test\\java\\apk\\Diet_meal.apk");
+        capabilities.setCapability("autoGrantPermissions", true); // set auto accept permission request setting
+        capabilities.setCapability("autoAcceptAlerts", true); // set auto accept all possible appearing alert
+
+        // set driver
+        URL url = new URL(baseUrl);
+        driver = new AndroidDriver(url, capabilities); // open app
+
+        // wait to prevent errors
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+        // process flow
+        // input name, weight, height
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/et_name")).sendKeys("Mike");
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/et_weight")).sendKeys("75");
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/et_height")).sendKeys("175");
+        // Click on male radio button
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/rb_male")).click();
+        // Click on next button
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/bt_next")).click();
+        // Click option radio button
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/rb_medium")).click();
+        // Click on finish button
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/bt_finish")).click();
+
+        // assertion
+        driver.findElement(By.id("com.fghilmany.dietmealapp:id/tv_header_name")).isDisplayed();
+
+        // close app
+        driver.quit();
+    }
+}
